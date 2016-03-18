fa1ab9c902605e2d8a0d16effd3ebfa6986fa0b1
Anonymous Candidate <anon@example.com>
2016-03-18 13:14:39 -0400

    Complete step 2

--- dolist/project.clj
+++ dolist/project.clj
@@ -1,9 +1,11 @@
 (defproject dolist "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Hello world Clojure Web app"
+  :url "http://example.com"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
+                 [compojure "1.1.1"]
+                 [ring/ring-defaults "0.2.0"]
                  [ring/ring-core "1.4.0"]
                  [ring/ring-jetty-adapter "1.4.0"]
                  [ring.middleware.logger "0.5.0"]
--- dolist/src/dolist/core.clj
+++ dolist/src/dolist/core.clj
@@ -3,18 +3,20 @@
         ring.middleware.resource
         ring.middleware.content-type
         ring.middleware.not-modified)
-  (:require [ring.middleware.logger :as logger]))
+  (:require [ring.middleware.logger :as logger]
+            [compojure.core :refer :all]
+            [compojure.route :as route]
+            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
 
-(def x 42)
+(defroutes app-routes
+  (GET "/" [] "<h1>Ford to City: Drop Dead</h1>
+                 <img src='uconst.gif' alt='Under Construction'</img>")
+  (route/not-found "Endpoint Not Found"))
 
-(defn handler [request]
-  (-> (response "<h1>Ford to City: Drop Dead</h1>
-          <img src='uconst.gif' alt='Under Construction'</img>")
-      (content-type "text/html")))
 
 (def loghandlerzz
- (-> handler
-     (logger/wrap-with-logger
-          :debug (fn [x] (print x)))
+   (wrap-defaults app-routes site-defaults)
+  #_ (-> handler
+     (logger/wrap-with-logger)
      (wrap-resource "public")
      ))
